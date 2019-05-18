9ff7e77b62c5167eee99a6a581534d8abd2c0dad
Liuml <951442843@qq.com>
2019-05-18 22:57:56 +0800

    选择排序

---
+++ Algroithms/script.js
@@ -0,0 +1,45 @@
+var srcArray;
+
+function resetValue() {
+    srcArray = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+    document.getElementById("demo").innerHTML = srcArray;
+}
+
+/**
+ * 选择排序
+ */
+function selectSort() {
+    for (let i = 0; i < srcArray.length - 1; i++) {
+        var min = i;
+        for (let j = i + 1; j < srcArray.length; j++) {
+            let temp = 0;
+            if (srcArray[j] < srcArray[min]) {
+                min = j;
+            }
+        }
+        swap(srcArray,min, i);
+    }
+    for (let i = 0; i < srcArray.length; i++) {
+        console.log(srcArray[i])
+    }
+
+    document.getElementById("demo").innerHTML = srcArray;
+}
+
+/**
+ * 插入排序
+ */
+function insterSort() {
+    document.getElementById("demo").innerHTML = Date();
+}
+
+/**
+ * 交换
+ * @param min
+ * @param i
+ */
+function swap(arr,min, i) {
+    let temp = arr[min];
+    arr[min] = arr[i];
+    arr[i] = temp;
+}
