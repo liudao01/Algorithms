0ff3b4f54a91c461abd384cc9c5c2814d2dc8a09
liuml <liudao1@qq.com>
2019-05-31 15:12:53 +0800

    归并排序

--- Algroithms/script.js
+++ Algroithms/script.js
@@ -85,124 +85,137 @@ function f(n) {
     if (n == 1) return 1;
     return n + f(n - 1);
 }
-var arr = [1, 4, 7, 8, 3, 6, 9];
+
 /**
  * 归并排序
  */
-function mergeSort() {
-
-    // let mid = Math.floor(arr.length / 2);
-    // console.log("中间: " + mid)
-    // merge(arr, 0, mid, arr.length - 1);
-
-    //正式
-    sort( 0, arr.length - 1);
-}
-
-function sort( left, right) {
-
-    if (left == right) {
-        return;
-    }
-    let m = left + right;
-    console.log("left: " + left+"   right = "+right);
-
-    //分成两半
-    let mid = Math.floor(m / 2);
-
-    console.log("中点: " + mid);
-    // let mid = Math.floor((left + right) / 2);
-    //左边排序
-    sort( left,  mid);
-
-    //右边排序
-    sort( mid+1, right);
-
-    merge( left, mid + 1, right);
-}
-
-/**
- *
- * @param arr
- * @param left 左边界
- * @param mid
- * @param right 右边界
- * @param temp
- */
-function merge( left, mid, right) {
-    let temp = new Array();
-    let i = left;//左序列指针
-    let j = mid + 1;//右序列指针
-    let k = 0;//临时数组指针
-
-    while (i <= mid && j <= right) {
-        console.log("数组排序中: " + temp);
-        if (arr[i] <= arr[j]) {
-            temp[k] = arr[i];
-            i++;
-            k++;
-        } else {
-            temp[k] = arr[j];
-            j++;
-            k++;
-        }
-    }
-    //左边有遗留
-    while (i <= mid) {//将左边剩余元素填充进temp中
-        temp[k++] = arr[i++];
-    }
-    //右边有剩下
-    while (j <= right) {//将右序列剩余元素填充进temp中
-        temp[k++] = arr[j++];
-    }
-    k = 0;
-    //将temp中的元素全部拷贝到原数组中
-    console.log("数组 " + temp);
-    // while(left <= right){
-    //     arr[left++] = temp[k++];
-    // }
-}
-
-
+// function mergeSort() {
 //
+//     // var arr = [1, 4, 7, 8, 3, 6, 9];
+//     // let mid = Math.floor(arr.length / 2);
+//     // console.log("中间: " + mid)
+//     // merge(arr, 0, mid, arr.length - 1);
 //
+//     //正式
+//     var arr = [8,4,5,7,1,3,6,2];
+//     // var arr = [9, 6, 11, 3, 5, 12, 8, 7, 10, 15, 14, 4, 1, 13, 2];
+//     sort(arr, 0, arr.length - 1);
+// }
+//
+// function sort(arr, left, right) {
+//
+//     if (left == right) {
+//         return;
+//     }
+//     console.log("left: " + left + "   right = " + right);
+//
+//     //分成两半
+//     let m = left + right;
+//     let mid = Math.floor(m / 2);
+//
+//     console.log("中点: " + mid);
+//     // let mid = Math.floor((left + right) / 2);
+//     //左边排序
+//     sort(arr, left, mid);
+//
+//     //右边排序
+//     sort(arr, mid + 1, right);
+//
+//     merge(arr, left, mid, right);
+// }
 //
 // /**
-//  * 归并排序
+//  *
+//  * @param arr
+//  * @param left 左边界
+//  * @param mid
+//  * @param right 右边界
+//  * @param temp
 //  */
-// function merge(leftArr, rightArr) {
-//     var result = [];
-//     while (leftArr.length > 0 && rightArr.length > 0) {
-//         if (leftArr[0] < rightArr[0])
-//             result.push(leftArr.shift()); //把最小的最先取出，放到结果集中
-//         else
-//             result.push(rightArr.shift());//shift() 方法从数组中删除第一个元素，并返回该元素的值。此方法更改数组的长度。
+// function merge(arr, left, mid, right) {
+//     let temp = new Array();
+//     let i = left;//左序列指针
+//     let j = mid + 1;//右序列指针
+//     let k = 0;//临时数组指针
+//
+//     while (i <= mid && j <= right) {
+//         console.log("数组排序中: " + temp);
+//         if (arr[i] <= arr[j]) {
+//             temp[k] = arr[i];
+//             i++;
+//             k++;
+//         } else {
+//             temp[k] = arr[j];
+//             j++;
+//             k++;
+//         }
 //     }
-//     let arr = result.concat(leftArr).concat(rightArr);
-//     return arr;  //剩下的就是合并，这样就排好序了
-//     //concat() 方法用于合并两个或多个数组。此方法不会更改现有数组，而是返回一个新数组。
+//     //左边有遗留
+//     while (i <= mid) {//将左边剩余元素填充进temp中
+//         temp[k++] = arr[i++];
+//     }
+//     //右边有剩下
+//     while (j <= right) {//将右序列剩余元素填充进temp中
+//         temp[k++] = arr[j++];
+//     }
+//     k = 0;
+//     //将temp中的元素全部拷贝到原数组中
+//     console.log("数组 " + temp);
+//     // 将排序后的元素，全部都整合到数组a中。
+//     for (i = 0; i < k; i++) {
+//         arr[start + i] = temp[i];
+//     }
+//
 // }
-//
-// function mergeSort() {
-//     // let array = srcArray;
-//     console.log("排序前数组 = " + srcArray)
-//     let arr = sort(srcArray);
-//     console.log("排序后数组 = " + arr)
-// }
-//
-// function sort(array) {
-//
-//     if (array.length == 1) return array;
-//     var middle = Math.floor(array.length / 2);       //求出中点
-//     var left = array.slice(0, middle);               //分割数组
-//     var right = array.slice(middle);
-//
-//     //左边排序
-//     let leftarr = sort(left);
-//     //右边排序
-//     let rightarr = sort(right);
-//     return merge(leftarr, rightarr); //递归合并与排序
-// }
+
+
+/**
+ * 归并排序
+ */
+function merge(leftArr, rightArr) {
+    var result = [];
+    while (leftArr.length > 0 && rightArr.length > 0) {
+        if (leftArr[0] < rightArr[0])
+            result.push(leftArr.shift()); //把最小的最先取出，放到结果集中
+        else
+            result.push(rightArr.shift());//shift() 方法从数组中删除第一个元素，并返回该元素的值。此方法更改数组的长度。
+    }
+    let arr = result.concat(leftArr).concat(rightArr);
+    // console.log("排序中: "+arr)
+    return arr;  //剩下的就是合并，这样就排好序了
+    //concat() 方法用于合并两个或多个数组。此方法不会更改现有数组，而是返回一个新数组。
+}
+
+
+function sort(array) {
+
+    if (array.length == 1) return array;
+    var middle = Math.floor(array.length / 2);       //求出中点
+    var left = array.slice(0, middle);               //分割数组
+    var right = array.slice(middle);
+
+    //左边排序
+    let leftarr = sort(left);
+    //右边排序
+    let rightarr = sort(right);
+    return merge(leftarr, rightarr); //递归合并与排序
+}
+
+
+function mergeSort() {
+    // let array = srcArray;
+    // console.log("排序前数组 = " + srcArray)
+    // let arr = sort(srcArray);
+    // console.log("排序后数组 = " + arr)
+
+    var aarr = [1, 4, 7, 8, 3, 6, 9];
+    let left = [1, 4, 7, 8];
+    let right = [3, 6, 9];
+    let arr = merge(left, right);
+    console.log("排序后数组 = " + arr)
+}
+
+
 /**
  * 交换
  * @param min
